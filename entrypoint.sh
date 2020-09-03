ad5cd8fe6a526dbd21ea9868f2dceec75e259388
Aleksey Covacevice <alecov@mail.com>
2020-09-03 01:23:55 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,36 @@
 #!/bin/bash
 
 set -e
+: ${SERVER:=1}
+: ${CONFIG:=/etc/bitcoin.conf}
+: ${DATADIR:=/var/lib/bitcoin}
+: ${RPCBIND:=$([[ -n $RPCUSER && -n $RPCPASSWORD || -n $RPCAUTH ]] && echo 0)}
+: ${RPCALLOWIP:=$([[ -n $RPCUSER && -n $RPCPASSWORD || -n $RPCAUTH ]] && echo 0/0)}
+
 cat <<-:: >/etc/bitcoin.conf
-	server=1
+	${SERVER:+server=$SERVER}
 	${TESTNET:+testnet=$TESTNET}
 	${TXINDEX:+txindex=$TXINDEX}
-	${RPCPORT:+rpcport=$RPCPORT}
 	${RPCUSER:+rpcuser=$RPCUSER}
 	${RPCAUTH:+rpcauth=$RPCAUTH}
+	${RPCALLOWIP:+rpcallowip=$RPCALLOWIP}
 	${RPCPASSWORD:+rpcpassword=$RPCPASSWORD}
+	${ALERTNOTIFY:+alertnotify=$ALERTNOTIFY}
+	${BLOCKNOTIFY:+blocknotify=$BLOCKNOTIFY}
+	${WALLETNOTIFY:+walletnotify=$WALLETNOTIFY}
+
+	[main]
+	${RPCPORT:+rpcport=$RPCPORT}
+	${RPCBIND:+rpcbind=$RPCBIND}
+
+	[test]
+	${RPCPORT:+rpcport=$RPCPORT}
+	${RPCBIND:+rpcbind=$RPCBIND}
+
+	[regtest]
+	${RPCPORT:+rpcport=$RPCPORT}
+	${RPCBIND:+rpcbind=$RPCBIND}
 ::
 
-mkdir -p /var/lib/bitcoin
-exec bitcoind -conf=/etc/bitcoin.conf -datadir=/var/lib/bitcoin "$@"
+mkdir -p "$DATADIR"
+exec bitcoind -conf="$CONFIG" -datadir="$DATADIR" "$@"
